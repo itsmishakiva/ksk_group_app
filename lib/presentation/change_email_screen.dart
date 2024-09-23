f36f154e17b96969fdf62e87a09004a1ed39f0e8
podleaver <plihta99@mail.ru>
2024-09-24 02:43:25 +0300

    fixed files name

--- lib/presentation/change_email_screen.dart
+++ lib/presentation/change_email_screen.dart
@@ -2,8 +2,8 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
 @RoutePage()
-class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+class ChangeEmailsScreen extends StatelessWidget {
+  const ChangeEmailsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
--- lib/presentation/recover_pass_screen.dart
+++ lib/presentation/recover_pass_screen.dart
@@ -2,8 +2,8 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
 @RoutePage()
-class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+class ChangePasssScreen extends StatelessWidget {
+  const ChangePasssScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
