d464cbcb6ccb7525b82f9050bca612672d5f32ce
itsmishakiva <itsmishakiva@outlook.com>
2024-10-15 03:06:11 +0300

    Refactoring

--- lib/features/authorization/presentation/sign_up/sign_up_screen.dart
+++ lib/features/authorization/presentation/sign_up/sign_up_screen.dart
@@ -13,6 +13,7 @@ import 'package:ksk_group/router/app_router.dart';
 
 part 'widgets/sign_in_button.dart';
 
+@RoutePage()
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({super.key});
 
